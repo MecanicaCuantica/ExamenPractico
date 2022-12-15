ef83176418c0295a2811d94aec1f16ad5afe53a6
Needamonk <Yo>
2022-12-15 18:06:18 -0500

    Login 3 Actividades

---
+++ app/src/main/java/com/example/examenpractico/Home.java
@@ -0,0 +1,62 @@
+package com.example.examenpractico;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Home extends AppCompatActivity {
+
+    private CheckBox tarea1, tarea2, tarea3, tarea4;
+    private Button siguiente;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        tarea1 = findViewById(R.id.boxRespuesta1);
+        tarea2 = findViewById(R.id.boxRespuesta2);
+        tarea3 = findViewById(R.id.boxFalso1);
+        tarea4 = findViewById(R.id.boxFalso2);
+        siguiente = findViewById(R.id.btnSiguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tarea1.isChecked() && tarea2.isChecked() && tarea3.isChecked() && tarea4.isChecked()){
+                    Intent i = new Intent(Home.this, Final.class);
+                    startActivity(i);
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Completa TODAS LAS TAREAS!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void cerrarSesion(View l) {
+
+
+    }
+}
