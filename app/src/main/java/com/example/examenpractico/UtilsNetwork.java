5e1269ebc4fa77679416101e344a660104367bcd
Damian1007 <80357420+Damian1007@users.noreply.github.com>
2022-12-19 18:54:10 -0500

    Utilities Network

--- app/src/main/java/com/example/examenpractico/UtilsNetwork.java
+++ app/src/main/java/com/example/examenpractico/UtilsNetwork.java
@@ -4,5 +4,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class UtilsNetwork {
-
+  public static boolean isOnline(Home context) {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(context.CONNECTIVITY_SERVICE);
+        if(cm != null){
+            NetworkInfo ni = cm.getActiveNetworkInfo();
+            if(ni != null){
+                return ni.isConnected();
+            }
+        }
+        return false;
+    }
 }
